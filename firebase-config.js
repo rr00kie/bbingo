df0d99813e3d507f38e6284d1098d4dc55f6d5a9
rr00kie <49950915+rr00kie@users.noreply.github.com>
2025-07-25 07:14:30 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Console에서 복사한 구성 객체를 여기에 붙여넣으세요.
+// 중요: 이 파일은 공개 저장소에 올리면 안 됩니다.
+window.firebaseConfig = {
+   apiKey: "AIzaSyDBpNQPgeNo33LczJPYCt1l-igSuD7CXxc",
+  authDomain: "bingo-f4d50.firebaseapp.com",
+  databaseURL: "https://bingo-f4d50-default-rtdb.firebaseio.com",
+  projectId: "bingo-f4d50",
+  storageBucket: "bingo-f4d50.firebasestorage.app",
+  messagingSenderId: "872227813426",
+  appId: "1:872227813426:web:4cbaafc8e45dfb4825e42b"
+};
